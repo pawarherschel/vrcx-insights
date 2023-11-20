a90885267d8f9c53ed8113b72a5a84331698f808
pawarherschel <pawarherschel@gmail.com>
2023-11-20 19:57:28 +0530

    pruning

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 pub mod zaphkiel {
     pub mod db;
-    pub mod error;
     pub mod gamelog_join_leave;
     pub mod group_access_type;
     pub mod join_leave_event;
     pub mod macros;
-    // pub mod trustfall;
     pub mod utils;
     pub mod vertex;
     pub mod world_instance;
