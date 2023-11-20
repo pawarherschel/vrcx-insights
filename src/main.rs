a7da121e27a88f78c8fd1a153e374249fcff8644
pawarherschel <pawarherschel@gmail.com>
2023-11-20 19:30:14 +0530

    added "analysis" part

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, RwLock};
+use std::time::Instant;
 
 use async_compat::Compat;
+use indicatif::style::ProgressTracker;
 use indicatif::ParallelProgressIterator;
 use rayon::prelude::*;
+use ron::ser::to_writer_pretty;
 use smol::prelude::*;
 use sqlx::SqlitePool;
 
@@ -10,74 +14,159 @@ use vrcx_insights::time_it;
 use vrcx_insights::zaphkiel::db::establish_connection;
 use vrcx_insights::zaphkiel::gamelog_join_leave::{GamelogJoinLeave, GamelogJoinLeaveRow};
 use vrcx_insights::zaphkiel::utils::get_pb;
-use vrcx_insights::zaphkiel::vertex::Vertex;
 use vrcx_insights::zaphkiel::world_instance::WorldInstance;
 
+static OWNER_ID: &str = "usr_c2a23c47-1622-4b7a-90a4-b824fcaacc69";
+static OWNER_NAME: &str = "Kat Sakura";
+
 fn main() {
-    let conn = time_it!("establishing connection to database" => smol::block_on(async {establish_connection().await}));
+    let start = Instant::now();
 
-    let test_name = "Kat Sakura".to_string();
+    let conn = time_it!(at once | "establishing connection to database" => smol::block_on(async {establish_connection().await}));
 
-    let user_id = get_uuid_of(test_name);
+    let cache = Arc::new(RwLock::new(HashMap::new()));
 
-    let locations = get_locations_for(user_id.clone(), &conn);
+    let user_id = time_it!(at once | format!("getting uuid of {}", OWNER_NAME.clone()) => get_uuid_of(OWNER_NAME.clone().into(), &conn));
 
-    let others = get_others_for(user_id.clone(), &conn, locations);
+    let latest_name = time_it!(at once | format!("getting latest name of {}", user_id.clone()) => get_display_name_for(user_id.clone(), &conn, cache.clone()));
 
-    let mut root = Vertex::new(user_id);
-    root.everyone_else = others
+    let locations = time_it!(at once | "getting the locations the user was in" => get_locations_for(user_id.clone(), &conn));
+
+    let others = time_it!(at once | "finding out the other users the user has seen" => get_others_for(user_id.clone(), &conn, locations, ));
+
+    let others_names = time_it!(at once | "getting names for other users" => others
         .par_iter()
-        .progress_with(get_pb(others.len() as u64, "Generating graph"))
-        .map(|(user_id, count)| (Vertex::new(user_id.clone()), *count))
-        .map(|(vertex, count)| {
-            let locations = get_locations_for(vertex.user_id.clone(), &conn);
-            let others = get_others_for(vertex.user_id.clone(), &conn, locations);
-            let mut vertex = vertex;
-            vertex.everyone_else = others
-                .par_iter()
-                .map(|(user_id, count)| (Vertex::new(user_id.clone()), *count))
-                .collect();
-            (vertex, count)
+        .progress_with(get_pb(others.len() as u64, "Getting names"))
+        .map(|(user_id, count)| {
+            (
+                get_display_name_for(user_id.clone(), &conn, cache.clone()),
+                *count,
+            )
         })
-        .collect::<HashMap<_, _>>();
+        .collect::<HashMap<_, _>>());
 
-    // let mut vertices = HashSet::new();
-    // vertices.insert(root.clone());
-    // root.everyone_else.iter().for_each(|(vertex, _)| {
-    //     vertices.insert(vertex.clone());
-    //     vertex.everyone_else.iter().for_each(|(vertex, _)| {
-    //         vertices.insert(vertex.clone());
-    //     });
+    let graph = Arc::new(RwLock::new(HashMap::new()));
+    graph
+        .clone()
+        .write()
+        .unwrap()
+        .insert(latest_name.clone(), others_names.clone());
+
+    time_it!(at once | "generating the graph" => others
+    .par_iter()
+    .progress_with(get_pb(others.len() as u64, "Generating graph"))
+    .for_each(|(user_id, _)| {
+        let latest_name = get_display_name_for(user_id.clone(), &conn, cache.clone());
+        let locations = get_locations_for(user_id.clone(), &conn);
+        let others = get_others_for(user_id.clone(), &conn, locations);
+        let others = others
+            .par_iter()
+            .map(|(user_id, count)| {
+                (
+                    get_display_name_for(user_id.clone(), &conn, cache.clone()),
+                    *count,
+                )
+            })
+            .collect::<HashMap<_, _>>();
+        graph.clone().write().unwrap().insert(latest_name, others);
+    }));
+
+    to_writer_pretty(
+        std::fs::File::create("graph.ron").unwrap(),
+        &graph,
+        Default::default(),
+    )
+    .unwrap();
+
+    let graph2 = time_it!(at once | "filtering graph" => graph
+        .clone()
+        .read()
+        .unwrap()
+        .par_iter()
+        .progress_with(get_pb(
+            graph.read().unwrap().len() as u64,
+            "Filtering graph",
+        ))
+        .filter_map(|a| {
+            let (name, others) = a;
+            let total = others.values().sum::<u32>();
+            let max = *others.values().max().unwrap() + 1;
+            let new_others = others
+                .par_iter()
+                .filter_map(|(k, count)| {
+                    let percentage = *count as f64 / total as f64 * 100_f64;
+                    let percentile = *count as f64 / max as f64 * 100_f64;
+                    if percentile > 50_f64 || percentage > 5_f64 {
+                        Some((k.clone(), (*count, (percentage * 100_f64).round() / 100_f64, (percentile * 100_f64).round() / 100_f64)))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<HashMap<String, _>>();
+            if new_others.is_empty() {
+                None
+            } else {
+                Some((name.clone(), new_others))
+            }
+        })
+        .collect::<HashMap<String, HashMap<String, _>>>());
+
+    let mut graph2_sorted = time_it!(at once | "duplicating graph" => graph2
+        .par_iter()
+        .progress_with(get_pb(graph2.len() as u64, "duplicating graph"))
+        .map(|(k, v)| (k.clone(), v.clone()))
+        .collect::<Vec<_>>());
+
+    time_it!("sorting graph by weighing adjacency list" => graph2_sorted.sort_by(|a, b| {
+        let (_, a) = a;
+        let (_, b) = b;
+        let a_len = a.iter().map(|(_, (count, _, _))| count).sum::<u32>();
+        let b_len = b.iter().map(|(_, (count, _, _))| count).sum::<u32>();
+        b_len.cmp(&a_len)
+    }));
+
+    time_it!("todo!(\"sorting adjacency list by values\")" => {
+
+    });
+
+    to_writer_pretty(
+        std::fs::File::create("graph2_sorted.ron").unwrap(),
+        &graph2_sorted,
+        Default::default(),
+    )
+    .unwrap();
+
+    // let undirected_graph = time_it!("convert the directed graph into an undirected graph" => {
+    //     let mut adjacency_matrix = HashMap::new();
+    //     for (name, others) in graph2_sorted.iter() {
+    //         let mut current_list = adjacency_matrix.entry(name.clone()).or_insert_with(HashSet::new).clone();
+    //         for (other, _) in others.iter() {
+    //             current_list.insert(other.clone());
+    //         }
+    //
+    //         for other in current_list.iter() {
+    //             let mut other_list = adjacency_matrix.entry(other_name.clone()).or_insert_with(HashSet::new).clone();
+    //             other_list.insert(name.clone());
+    //             adjacency_matrix.insert(other_name.clone(), other_list);
+    //         }
+    //
+    //         adjacency_matrix.insert(name.clone(), current_list.clone());
+    //     }
+    //
+    //     adjacency_matrix
     // });
-
-    // let dumb_vertices = vertices
-    //     .clone()
-    //     .iter()
-    //     .map(|v| Vertex::new(get_display_name_for(v.user_id.clone())))
-    //     .collect::<Vec<_>>();
     //
-    // println!("vertices = {:#?}", vertices);
-    //
-    // let mut file = std::fs::File::create("graph.ron").unwrap();
-    // to_writer_pretty(&mut file, &dumb_vertices, Default::default()).unwrap();
+    // to_writer_pretty(
+    //     std::fs::File::create("undirected_graph.ron").unwrap(),
+    //     &undirected_graph,
+    //     Default::default(),
+    // )
+    // .unwrap();
 
-    // let others = time_it!("sorting others by count" => {
-    //     let mut others = others.into_iter().collect::<Vec<_>>();
-    //     others.sort_by(|a, b| b.1.cmp(&a.1));
-    //     others
-    // });
-    //
-    // let others = others
-    //     .iter()
-    //     .map(|(user_id, count)| (get_display_name_for(user_id.clone()), count))
-    //     .collect::<Vec<_>>();
-
-    // println!("top 10: {:#?}", others.iter().take(10).collect::<Vec<_>>());
-
-    // debug!(others);
+    println!("\x07Total run time => {:?}", start.elapsed());
 }
 
-pub fn get_uuid_of(display_name: String) -> String {
+pub fn get_uuid_of(display_name: String, pool: &SqlitePool) -> String {
     let q = "select *
         from gamelog_join_leave
         where display_name like ?
@@ -88,7 +177,7 @@ pub fn get_uuid_of(display_name: String) -> String {
             async {
                 sqlx::query_as::<_, GamelogJoinLeaveRow>(q)
                     .bind(display_name.clone())
-                    .fetch_one(&establish_connection().await).await
+                    .fetch_one(pool).await
             }
         ).unwrap()
     });
@@ -100,21 +189,33 @@ pub fn get_uuid_of(display_name: String) -> String {
     row.user_id
 }
 
-pub fn get_display_name_for(user_id: String) -> String {
+pub fn get_display_name_for(
+    user_id: String,
+    pool: &SqlitePool,
+    cache: Arc<RwLock<HashMap<String, String>>>,
+) -> String {
+    if let Some(display_name) = cache.read().unwrap().get(&user_id) {
+        return display_name.clone();
+    }
+
     let q = "select *
         from gamelog_join_leave
         where user_id like ?
         order by created_at desc
         limit 1";
 
-    smol::block_on(async {
+    let name = smol::block_on(async {
         sqlx::query_as::<_, GamelogJoinLeaveRow>(q)
             .bind(user_id.clone())
-            .fetch_one(&establish_connection().await)
+            .fetch_one(pool)
             .await
     })
     .unwrap()
-    .display_name
+    .display_name;
+
+    cache.write().unwrap().insert(user_id, name.clone());
+
+    name
 }
 
 pub fn get_locations_for(user_id: String, conn: &SqlitePool) -> HashSet<WorldInstance> {
@@ -130,7 +231,7 @@ pub fn get_locations_for(user_id: String, conn: &SqlitePool) -> HashSet<WorldIns
     })
     .unwrap();
 
-    rows.iter()
+    rows.par_iter()
         .cloned()
         .map(|row| row.into())
         .filter_map(|row: GamelogJoinLeave| row.location)
@@ -142,46 +243,41 @@ pub fn get_others_for(
     conn: &SqlitePool,
     locations: HashSet<WorldInstance>,
 ) -> HashMap<String, u32> {
-    let others = time_it!(at once | format!("processing {} locations", locations.len()) => {
-    locations
+    let others = locations
         .par_iter()
-        .progress_with(get_pb(locations.len() as u64, "Processing locations"))
         .map(|location| {
-            let q =
-                "select *
+            let q = "select *
                     from gamelog_join_leave
                     where location like ?";
 
             let prefix = location.get_prefix();
             let location = format!("%{}%", prefix);
 
-            let rows = smol::block_on(
-                Compat::new(
-                async {
-                    sqlx::query_as::<_, GamelogJoinLeaveRow>(q)
-                        .bind(location)
-                        .fetch_all(conn).await
-                })
-            ).unwrap();
-
+            let rows = smol::block_on(Compat::new(async {
+                sqlx::query_as::<_, GamelogJoinLeaveRow>(q)
+                    .bind(location)
+                    .fetch_all(conn)
+                    .await
+            }))
+            .unwrap();
 
             let rows = rows
-                .iter()
+                .par_iter()
                 .cloned()
                 .map(|row| row.into())
                 .collect::<Vec<GamelogJoinLeave>>();
 
-
-            rows.iter()
+            rows.par_iter()
                 .filter(|row| {
-                    row.location.is_some() && row.user_id.is_some() && row.user_id.clone().unwrap() != user_id
+                    row.location.is_some()
+                        && row.user_id.is_some()
+                        && row.user_id.clone().unwrap() != user_id
                 })
                 .map(|row| row.user_id.clone().unwrap().clone())
                 .collect::<HashSet<_>>()
         })
         .filter(|other| !other.is_empty())
-        .collect::<Vec<_>>()
-    });
+        .collect::<Vec<_>>();
 
     let mut everyone_else = HashMap::new();
 
